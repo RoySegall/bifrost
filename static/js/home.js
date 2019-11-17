75f2ec7f496a5ab7494aa7b44311a709ec998b2f
Roy Segall <roy@segall.io>
2019-11-17 11:18:26 +0200

    Display all the items in the front page.

--- static/js/home.js
+++ static/js/home.js
@@ -2,47 +2,61 @@ class App extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {flights: []};
+        this.state = {
+            timelines: [],
+            loading: false
+        };
     }
 
-    componentDidMount() {
-        this.fetchData()
+    async componentDidMount() {
+        await this.fetchData()
     }
 
-    getCookie = () =>  function(name) {
-        var r = document.cookie.match("\\b" + name + "=([^;]*)\\b");
+    getCookie(name) {
+        const r = document.cookie.match("\\b" + name + "=([^;]*)\\b");
         return r ? r[1] : null;
     };
 
+    fetchData = async () => {
+        this.setState({loading: true});
 
-    fetchData = () => {
-        this.setState({loading: true}, () => {
-            fetch('graphql/', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'X-CSRFToken': this.getCookie('csrftoken')
-                },
-                body: JSON.stringify({
-                    query: `query { todoItems { id text completed } }`,
-                }),
-            })
-                .then(response => {
-                    return response.json()
-                })
-                .then(responseAsJson => {
-                    this.setState({loading: false, data: responseAsJson.data})
-                })
-        })
-    }
+        const response = await fetch('graphql/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': this.getCookie('csrftoken'),
+            },
+            body: JSON.stringify({
+                query: `query { timelines { title, id, startingDate, endingDate } }`,
+            }),
+        });
+
+        const results = await response.json();
+        this.setState({loading: false, timelines: results.data.timelines})
+    };
 
     render() {
-        return (
-            <div className="flights">
-                {this.state.flights}
+        if (this.state.loading) {
+            return <div className="text-center">
+                <i className="loading fad fa-spinner fa-spin"></i>
             </div>
-        );
+        }
+
+        return <Timelines timelines={this.state.timelines} />
     }
+}
+
+function Timelines(data) {
+    const {timelines} = data;
+
+    return timelines.map((timeline, key) => {
+        return <div className="row" key={key}>
+            <div className="col-12">
+                {timeline.title}. From {timeline.startingDate} to: {timeline.endingDate}
+            </div>
+        </div>
+    });
+
 }
 
 ReactDOM.render(
