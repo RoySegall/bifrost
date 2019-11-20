49ee8df0c5633e8552c5aaf5fdf2ea01c9e05594
Roy Segall <roy@segall.io>
2019-11-20 17:17:57 +0200

    Tiny bit of order.

--- travis/python_setup.sh
+++ travis/python_setup.sh
@@ -5,4 +5,6 @@ if [ $1 -eq 1 ]; then
     # When first argument is 1 we want a DB.
     mysql -e 'CREATE DATABASE bifrost;'
     cp bifrost/local_settings.travis.py bifrost/local_settings.py
+else
+    pip install pycodestyle
 fi
