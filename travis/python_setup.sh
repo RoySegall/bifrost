a4f07f5e15b9cce69336b61a37c7c6d79057eeec
Roy Segall <roy@segall.io>
2019-11-20 17:27:28 +0200

    Ooops

--- travis/python_setup.sh
+++ travis/python_setup.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
-if [[ $1 == 1 ]]; then
-    # When first argument is 1 we want a DB.
-    pip install -r requirements.txt
-    mysql -e 'CREATE DATABASE bifrost;'
-    cp bifrost/local_settings.travis.py bifrost/local_settings.py
-else
-    pip install pycodestyle
-fi
+pip install -r requirements.txt
+mysql -e 'CREATE DATABASE bifrost;'
+cp bifrost/local_settings.travis.py bifrost/local_settings.py
+pip install pycodestyle
