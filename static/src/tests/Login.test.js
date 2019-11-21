77a9cca31425faef2e69f93833ef6ec88576195b
Roy Segall <roy@segall.io>
2019-11-21 17:06:27 +0200

    Covering the login form. Login will be done in the integration section.

--- static/src/tests/Login.test.js
+++ static/src/tests/Login.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Login from '../Components/Login';
-import {shallow} from 'enzyme';
+import {shallow, mount} from 'enzyme';
 import {configure} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
@@ -9,7 +9,7 @@ configure({ adapter: new Adapter() });
 
 test('Testing the login component', () => {
 
-    const component = renderer.create(<Login />);
+    const component = renderer.create(<Login/>);
 
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
@@ -17,7 +17,7 @@ test('Testing the login component', () => {
 
 test('Testing the icon change as we focus on elements', () => {
 
-    const login = shallow(<Login />);
+    const login = shallow(<Login/>);
     expect(login.find('.icon i').hasClass('fa-sign-in')).toBe(true);
 
     login.find('#username').simulate('focus');
@@ -30,8 +30,35 @@ test('Testing the icon change as we focus on elements', () => {
 });
 
 test('Testing login form validation', () => {
-   const login = shallow(<Login />);
+    const login = mount(<Login/>);
+    const event = {
+        preventDefault: () => {
+        }
+    };
 
-   // login.find('.form-group.actions button').simulate('click');
+    // Mock the event for the the onclick button.
+    jest.spyOn(event, 'preventDefault');
+
+    // Click on the button and verify the event was triggered.
+    login.find('.form-group.actions button').simulate('click', event);
+    expect(event.preventDefault).toBeCalled();
+
+    // Verify we got the error.
+    expect(login.find('.alert').text()).toEqual('Username is required');
+
+    // Fill in the user, submit the form and make sure we got an error relate to the email.
+    login.setState({ username: 'bar' });
+    login.find('.form-group.actions button').simulate('click', event);
+
+    expect(login.find('.alert').text()).toEqual('Password is required');
+
+    // Setting the password.
+    login.setState({ password: 'bar' });
+
+    // Submitting the form.
+    login.find('.form-group.actions button').simulate('click', event);
+
+    // Making sure we got the spinner.
+    expect(login.find('.icon i').hasClass('fa-spinner')).toBe(true);
 
 });
