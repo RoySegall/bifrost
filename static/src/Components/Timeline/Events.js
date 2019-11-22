0dbbe025078d5e83d8092f6776d28ef9f2bb3625
Roy Segall <roy@segall.io>
2019-11-23 00:41:22 +0200

    Basic structure.

--- static/src/Components/Timeline/Events.js
+++ static/src/Components/Timeline/Events.js
@@ -114,6 +114,39 @@ export default class Events extends React.Component {
                     <div className="map">
                         <img className="img-fluid" src={process.env.PUBLIC_URL + '/mps.png'} />
                     </div>
+
+                    <div className="col-12 metadata">
+                        Location: <b>location title</b>, at <b>June 25th 14:00</b>
+
+                        <p>
+                            Lorem Ipsum is simply dummy text of the printing and
+                            typesetting industry. Lorem Ipsum has been the
+                            industry's standard dummy text ever since the 1500s,
+                            when an unknown printer took a galley of type and
+                            scrambled it to make a type specimen book. It has
+                            survived not only five centuries, but also the leap
+                            into electronic typesetting, remaining essentially
+                            unchanged. It was popularised in the 1960s with the
+                            release of Letraset sheets containing Lorem Ipsum
+                            passages, and more recently with desktop publishing
+                            software like Aldus PageMaker including versions of
+                            Lorem Ipsum.
+                        </p>
+                        <p>
+                            Lorem Ipsum is simply dummy text of the printing and
+                            typesetting industry. Lorem Ipsum has been the
+                            industry's standard dummy text ever since the 1500s,
+                            when an unknown printer took a galley of type and
+                            scrambled it to make a type specimen book. It has
+                            survived not only five centuries, but also the leap
+                            into electronic typesetting, remaining essentially
+                            unchanged. It was popularised in the 1960s with the
+                            release of Letraset sheets containing Lorem Ipsum
+                            passages, and more recently with desktop publishing
+                            software like Aldus PageMaker including versions of
+                            Lorem Ipsum.
+                        </p>
+                    </div>
                 </div>
 
             </div>
