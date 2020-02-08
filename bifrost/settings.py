eb5e60cd12f28669600e7d91f58124849530ce11
RoySegall <roy@segall.io>
2020-02-08 21:37:00 +0200

    Fixing the dpendancis

--- bifrost/settings.py
+++ bifrost/settings.py
@@ -150,7 +150,7 @@ STATICFILES_DIRS = [
 CSRF_TRUSTED_ORIGINS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
-CORS_ORIGIN_WHITELIST = ['*']
+CORS_ORIGIN_WHITELIST = ['http://*', 'https://*']
 FRONT_ADDRESS = "http://localhost:3000"
 
 from .local_settings import *
