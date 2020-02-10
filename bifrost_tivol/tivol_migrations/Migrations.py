89657b8b91a55b03bbc0fc19eb71a4b172c0dda3
RoySegall <roy@segall.io>
2020-02-10 07:07:08 +0200

    Addng more migration bae. Next - add content

--- bifrost_tivol/tivol_migrations/Migrations.py
+++ bifrost_tivol/tivol_migrations/Migrations.py
@@ -3,33 +3,46 @@ from tivol.base_classes.migration_handler_base import MigrationHandlerBase
 from os import getcwd, path
 from django.contrib.auth.models import User
 
+from bifrost_timeline.models import Timeline
 from bifrost_tivol.tivol_migrations.Plugins import IsSuperUserPlugin, \
     PasswordPlugin
 
 
-class BifrostUserMigration(MigrationHandlerBase):
+class MigrationBase(MigrationHandlerBase):
 
-    def init_metadata(self):
-        self.id = 'user'
-        self.name = 'User migration'
-        self.description = 'Migrating users into the system'
+    def init_helper(self, model_name, file, model):
+        self.id = model_name
+        self.name = f'{model_name.capitalize()} migration'
+        self.description = f'Migrating {model_name}s into the system'
 
         file_path = path.join(
             getcwd(),
             'bifrost_tivol',
             'tivol_migrations',
             'source_files',
-            'users.csv'
+            file
         )
 
         csv_mapper = CsvMapper()
         csv_mapper.set_destination_file(path=file_path)
 
         self.add_source_mapper(csv_mapper)
-        self.set_model_target(User)
+        self.set_model_target(model)
+
+
+class UserMigration(MigrationBase):
+
+    def init_metadata(self):
+        self.init_helper('user', 'users.csv', User)
 
         self.fields_plugins = {
             'password': [PasswordPlugin],
             'is_staff': [IsSuperUserPlugin],
             'is_superuser': [IsSuperUserPlugin]
         }
+
+
+class TimelineMigration(MigrationBase):
+
+    def init_metadata(self):
+        self.init_helper('timeline', 'timelines.csv', Timeline)
--- bifrost_tivol/tivol_migrations/entrypoint.py
+++ bifrost_tivol/tivol_migrations/entrypoint.py
@@ -1,9 +1,9 @@
 from tivol.base_classes.entry_point import EntryPoint
-from bifrost_tivol.tivol_migrations.Migrations import BifrostUserMigration
+from bifrost_tivol.tivol_migrations import Migrations
 
 
 class BifrostEntryPoint(EntryPoint):
 
     def register_migrations(self):
-        self.add_migration_handler(BifrostUserMigration)
-
+        self.add_migration_handler(Migrations.UserMigration)
+        self.add_migration_handler(Migrations.TimelineMigration)
