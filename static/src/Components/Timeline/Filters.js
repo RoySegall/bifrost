ff3fd9c5c3de1544a365abce40e9f9ba2181a700
Roy Segall <roy@segall.io>
2020-02-21 09:51:24 +0200

    Commit work on button

--- static/src/Components/Timeline/Filters.js
+++ static/src/Components/Timeline/Filters.js
@@ -4,43 +4,49 @@ import {Calendar, Car, Plane, Hotel, Friends} from '../../Fonts'
 
 export default class Filters extends React.Component {
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            filters: [
-                {'key': 'all', title: 'All events', icon: Calendar(), first: true},
-                {'key': 'pickingcarSet', title: 'Picking car', icon: Car()},
-                {'key': 'flightSet', title: 'Flight', icon: Plane()},
-                {'key': 'accommodationSet', title: 'Accommodation', icon: Hotel()},
-                {'key': 'meetingconjunctionSet', title: 'Meet employees', icon: Friends()},
-            ],
-            activeFilter: 'all',
-        };
-    }
+    this.state = {
+      filters: [
+        {'key': 'all', title: 'All events', icon: Calendar(), first: true},
+        {'key': 'pickingcarSet', title: 'Picking car', icon: Car()},
+        {'key': 'flightSet', title: 'Flight', icon: Plane()},
+        {'key': 'accommodationSet', title: 'Accommodation', icon: Hotel()},
+        {'key': 'meetingconjunctionSet', title: 'Meet employees', icon: Friends()},
+      ],
+      activeFilter: 'all',
+    };
+  }
 
-    switchFilter(filter) {
-        this.setState({activeFilter: filter});
-    }
+  switchFilter(filter) {
+    this.setState({activeFilter: filter});
+  }
 
-    List() {
-        return this.state.filters.map((item, key) => {
+  List() {
+    return this.state.filters.map((item, key) => {
 
-            const active = this.state.activeFilter === item['key'] ? 'selected' : '';
-            const first = item['first'] ? 'first' : '';
+      const bg = this.state.activeFilter === item['key'] ? 'bg-green-500' : 'bg-indigo-600';
+      const first = item['first'] ? 'first' : '';
 
-            return <li
-                key={key}
-                className={`pr-4 ${item['key']} ${active} ${first}`}
-                onClick={() => this.switchFilter(item['key'])}>
-                <span>{item['icon']} {item['title']}</span>
-            </li>
-        })
-    }
+      return <li
+        key={key}
+        onClick={() => this.switchFilter(item['key'])}>
+          <button className={`
+            text-gray-100 hover:text-gray-500 active:text-red-400
+            bg-indigo-600 active:bg-yellow-400
+            font-bold pt-2 pb-2 pl-4 pr-4 ml-2 rounded
+          `}>
+              <span>{item['icon']}</span>
+              <span className="pl-2">{item['title']}</span>
+          </button>
+      </li>
+    })
+  }
 
-    render() {
-        return <div>
-            <ul className="flex flex-row justify-start pt-4">{this.List()}</ul>
-        </div>
-    }
+  render() {
+    return <div>
+      <ul className="flex flex-row justify-start pt-4">{this.List()}</ul>
+    </div>
+  }
 }
