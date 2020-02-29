7f02c5fa59e459fca944b13f0bdfe48a348136d0
RoySegall <roy@segall.io>
2020-02-29 09:44:06 +0200

    Trying now

--- travis/e2e-setup.sh
+++ travis/e2e-setup.sh
@@ -2,7 +2,7 @@
 
 # Installing django stuff.
 
-echo "------"
+# Setting up the server.
 echo "Setting up dango stuff"
 pip install -r requirements.txt
 mysql -e 'CREATE DATABASE bifrost;'
@@ -10,7 +10,13 @@ cp bifrost/local_settings.travis.py bifrost/local_settings.py
 python manage.py migrate
 python manage.py runserver &
 echo "Server is runing!"
-echo "------"
+
+# Setting up the front.
+echo "Setting up react stuff"
+cd static
+npm i
+npm start &
+echo "React is running"
 
 
 
