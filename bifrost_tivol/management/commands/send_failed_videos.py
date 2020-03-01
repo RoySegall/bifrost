09baf25a584ceeec29fb87c5c41ff65f2f646ddf
RoySegall <roy@segall.io>
2020-03-01 06:23:18 +0200

    Trying now

--- bifrost_tivol/management/commands/send_failed_videos.py
+++ bifrost_tivol/management/commands/send_failed_videos.py
@@ -9,37 +9,48 @@ from sendgrid.helpers.mail import (
     FileType, Disposition, ContentId)
 import base64
 import os
+from os.path import join
 
 
 class Command(BaseCommand):
     help = 'Closes the specified poll for voting'
 
     def add_videos(self, message: Mail):
+        """
+        Adding videos to the email.
+        """
+        files = glob(join(os.getcwd(), 'static', 'cypress', 'videos', '*.mp4'))
 
-        files = glob(os.path.join(os.getcwd(), 'static', 'cypress', 'videos', '*.mp4'))
         for file in files:
             with open(file, 'rb') as f:
                 data = f.read()
                 f.close()
-            filename = file.split('/',)[-1]
+
+            filename = file.split('/', )[-1]
             encoded = base64.b64encode(data).decode()
             attachment = Attachment()
             attachment.file_content = FileContent(encoded)
             attachment.file_type = FileType('video/mp4')
-            attachment.file_name = FileName('_'.join(filename.split('.')[0:-1])+'.mp4')
+            attachment.file_name = FileName(
+                '_'.join(filename.split('.')[0:-1]) + '.mp4')
             attachment.disposition = Disposition('attachment')
             attachment.content_id = ContentId('Example Content ID')
 
             message.add_attachment(attachment)
 
     def add_picturs(self, message: Mail):
-        files = glob(os.path.join(os.getcwd(), 'static', 'cypress', 'screenshots', '*', '*.png'))
+        """
+        Adding the pictures to the email.
+        """
+        path = join(os.getcwd(),
+                    'static', 'cypress', 'screenshots', '*', '*.png')
+        files = glob(path)
 
         for file in files:
             with open(file, 'rb') as f:
                 data = f.read()
                 f.close()
-            filename = file.split('/',)[-1]
+            filename = file.split('/', )[-1]
             encoded = base64.b64encode(data).decode()
             attachment = Attachment()
             attachment.file_content = FileContent(encoded)
@@ -52,14 +63,26 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        if not os.environ['SEND_ON_FAIL']:
+        if 'SEND_ON_FAIL' not in os.environ:
+            # Don't send mail when the build is failing. For various reason but
+            # we won't mention them here.
             return
 
+        # Building the variables.
+        subject = f"{os.environ['TRAVIS_COMMIT_MESSAGE']} on " \
+                  f"{os.environ['TRAVIS_BRANCH']} is failing"
+        mail_content = f"For some reason, the commit has been failed. " \
+                       f"Screenshots and videos are attached. You can see " \
+                       f"the logs " \
+                       f"<a href='{os.environ['TRAVIS_BUILD_WEB_URL']}>" \
+                       f"here</a>."
+
+        # Building the email alongside the media of the email.
         message = Mail(
             from_email='from_email@example.com',
             to_emails='roy@segall.io',
-            subject=f"{os.environ['TRAVIS_COMMIT_MESSAGE']} on {os.environ['TRAVIS_BRANCH']} is failing",
-            html_content='For some reason, the commit has been failed. Screenshots and videos are attached',
+            subject=subject,
+            html_content=mail_content,
         )
 
         self.add_videos(message)
