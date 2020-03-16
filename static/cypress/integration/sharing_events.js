c575e88b8c47a231d56db5207fdf721016787c42
RoySegall <roy@segall.io>
2020-03-16 07:09:59 +0200

    Adding integration test

--- static/cypress/integration/sharing_events.js
+++ static/cypress/integration/sharing_events.js
@@ -6,5 +6,16 @@ describe('Sharing events', function() {
         cy.get('.text-3xl').should('contain', 'Trip to New York');
 
         // Testing the logic of the sharing events.
+        cy.contains('Trip to New York').click();
+
+        // Verify we should not have the flight events.
+        cy.contains('Flight to Frankfurt').should('not.be.visible');
+
+        // Logging as tony and verify the flight to frankfurt exists.
+        cy.login('tony', 'ilovepeper');
+        cy.visit('/');
+        cy.get('.text-3xl').should('contain', 'Trip to New York');
+        cy.contains('Trip to New York').click();
+        cy.contains('Flight to Frankfurt').should('be.visible');
     });
 });
